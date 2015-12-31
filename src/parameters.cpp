8917d6f140c3ec1dda9ced15ab0a3bcac2311250
Daniele De Sensi <d.desensi.software@gmail.com>
2015-12-31 15:11:07 +0100

    [FIX] Fixed parameters validation when contract is NONE.

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -519,6 +519,9 @@ Parameters::~Parameters(){
 
 ParametersValidation Parameters::validate(){
     ParametersValidation r = VALIDATION_OK;
+    if(contractType == CONTRACT_NONE){
+        return VALIDATION_OK;
+    }
     setDefaultPost();
 
     /** Validate frequency knob. **/
